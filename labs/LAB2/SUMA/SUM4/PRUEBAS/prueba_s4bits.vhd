805494974d1ef4efea5b30ffa3fb1de481ed6ade
jafeth <jafethrafael12@gmail.com>
2022-10-04 20:31:26 +0200

    ?

--- labs/LAB2/SUMA/SUM4/PRUEBAS/prueba_s4bits.vhd
+++ labs/LAB2/SUMA/SUM4/PRUEBAS/prueba_s4bits.vhd
@@ -22,14 +22,18 @@ port (A: in std_logic_vector(3 downto 0);
 	csal: out std_logic);
 end component;				
 
--- senyales	
-signal a, b, cen: std_logic;
+-- senyales inputs	
+signal a, b: std_logic_vector(3 downto 0);
+signal cen: std_logic;
+-- senyales outputs
+signal sum : std_logic_vector(3 downto 0);
+signal csal: std_logic;
 
 begin	
 -- instanciacion y estimulos
-a <= "UUUU", 0000 after 200 ns, "UUUU" after 400 ns, 0000 after 600 ns, "UUUU" after 800 ns, 0000 after 1000 ns, "UUUU" after 1200 ns;
-b <= "UUUU", 0000 after 200 ns, "UUUU" after 400 ns, 0100 after 600 ns, "UUUU" after 800 ns, 0100 after 1000 ns, "UUUU" after 1200 ns;
-cen <= "U", 0 after 200 ns, "U" after 400 ns, 0 after 600 ns, "U" after 800 ns, 1 after 1000 ns, "U" after 1200 ns;
-
+	S4bit0: S4bits port map(A=>a, B=>b, cen=>cen, csal=>csal, SUM=>sum);
+	a <= "UUUU", "0000" after 200 ns, "UUUU" after 400 ns, "0000" after 600 ns, "UUUU" after 800 ns, "0000" after 1000 ns, "UUUU" after 1200 ns;
+	b <= "UUUU", "0000" after 200 ns, "UUUU" after 400 ns, "0100" after 600 ns, "UUUU" after 800 ns, "0100" after 1000 ns, "UUUU" after 1200 ns;
+	cen <= 'U', '0' after 200 ns, 'U' after 400 ns, '0' after 600 ns, 'U' after 800 ns, '1' after 1000 ns, 'U' after 1200 ns;
 end prueba;		
 
